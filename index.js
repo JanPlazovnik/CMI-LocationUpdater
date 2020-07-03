21102dd3356c55e6a67c62e05a902542a7174149
Jan Plazovnik <jan.plazovnik@gmail.com>
2020-07-03 18:38:18 +0200

    add ability to easily change the world

--- index.js
+++ index.js
@@ -10,30 +10,32 @@ const { open } = require('sqlite');
     // The position to change every user location to
     // <world_name>:<x>:<y>:<z>:<pitch>:<yaw>
     const spawnPos = 'world:275:202:242:69:-278';
+    // The world to look for • Change this to the world you need to remove homes and locations from
+    const world = 'world_nether';
     // Get every user from the database
     const users = await db.all('SELECT * FROM users');
     for (const user of users) {
       let needsChange = false;
       let { id, username, LogOutLocation, DeathLocation, TeleportLocation, Homes } = user;
-      // Did the user logout in the nether?
-      if (LogOutLocation && LogOutLocation.includes('world_nether')) {
+      // Did the user logout in the world?
+      if (LogOutLocation && LogOutLocation.includes(world)) {
         LogOutLocation = spawnPos;
         needsChange = true;
       }
-      // Did the user die in the nether?
-      if (DeathLocation && DeathLocation.includes('world_nether')) {
+      // Did the user die in the world?
+      if (DeathLocation && DeathLocation.includes(world)) {
         DeathLocation = spawnPos;
         needsChange = true;
       }
-      // Is the users last teleport location in the nether?
-      if (TeleportLocation && TeleportLocation.includes('world_nether')) {
+      // Is the users last teleport location in the world?
+      if (TeleportLocation && TeleportLocation.includes(world)) {
         TeleportLocation = spawnPos;
         needsChange = true;
       }
       // Does the user have any active homes?
-      if(Homes) {
-        // Remove all homes in the nether
-        Homes = Homes.split(';').filter((home) => !home.includes('world_nether')).join(';');
+      if (Homes && Homes.includes(world)) {
+        // Remove all homes in the world
+        Homes = Homes.split(';').filter((home) => !home.includes(world)).join(';');
         needsChange = true;
       }
       // Prepare the UPDATE statement for the user with the new locations and homes
